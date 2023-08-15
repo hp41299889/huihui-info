891afc88680ed8151b3f98e8276d17da86b375e4
hp4129889 <hp4129889@gmail.com>
2023-08-15 09:38:02 +0800

    test: add test environment and header test

---
+++ app/component/layout/sider/messenger.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import Image from "next/image";
+import { Box, Stack } from "@mui/material";
+import Link from "next/link";
+
+import { messengers } from "./option";
+
+const Messenger: FC = () => {
+  return (
+    <Box position="fixed" bottom={24} right={24}>
+      <Stack rowGap={2}>
+        {messengers.map((m, i) => (
+          <Link key={`messenger_${i}`} href={m.href}>
+            <Image {...m} />
+          </Link>
+        ))}
+      </Stack>
+    </Box>
+  );
+};
+
+export default Messenger;
---
+++ app/component/layout/sider/option.ts
@@ -0,0 +1,20 @@
+import { ImageLinker } from "@/app/component/link/linker/interface";
+
+export const messengers: ImageLinker[] = [
+  {
+    alt: "facebook",
+    href: "https://www.facebook.com/profile.php?id=100094935322100",
+    src: "/icon/messenger.svg",
+    width: 64,
+    height: 64,
+    style: { cursor: "pointer" },
+  },
+  {
+    alt: "line",
+    href: "https://line.me/ti/p/nS3htUh-Zg",
+    src: "/icon/line.png",
+    width: 64,
+    height: 64,
+    style: { cursor: "pointer" },
+  },
+];
